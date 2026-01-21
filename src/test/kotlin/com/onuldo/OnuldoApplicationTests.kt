cccd63cb02d177029d4941e0f7f5b50bf68de0f7
강경원 <kwkang@ssrinc.co.kr>
2026-01-21 15:39:13 +0900

    initial Project

---
+++ src/test/kotlin/com/onuldo/OnuldoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.onuldo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OnuldoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
